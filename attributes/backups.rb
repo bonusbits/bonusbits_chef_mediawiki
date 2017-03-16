694573caa02498ce1a0292e68aa6f730eadb140a
Levon Becker <LevonBecker@users.noreply.github.com>
2017-03-16 13:59:53 -0700

    Release 1.0.3 (#21)
    * Added CloudFormation Parameters JSON Example. Updated content on README
    
    * Added cloud-init, cloud-init-output and yum logs to CloudWatch Logging
    
    * Removed draft backup script code. Going to use Data Pipeline solution to back EFS.
    
    * Removed update-tag because created own repo with the script https://github.com/bonusbits/update_reference_git_tag
    
    * Removed draft backup script code. Going to use Data Pipeline solution to back EFS.

--- attributes/backups.rb
+++
@@ -1,6 +0,0 @@
-default['bonusbits_mediawiki_nginx']['backups'].tap do |backups|
-  backups['configure'] = false
-  backups['bucket'] = 'bonusbits-backups'
-  backups['efs_backup_filename'] = 'bonusbits-efs.tgz'
-  backups['web_backup_filename'] = 'bonusbits-webcontent.tgz'
-end
