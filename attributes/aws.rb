594a310e674317b2305bee6433f462fc8d683611
Levon Becker <levon.becker.github@bonusbits.com>
2017-03-17 10:26:55 -0700

    A little more cleanup of unused attributes

--- attributes/aws.rb
+++ attributes/aws.rb
@@ -2,6 +2,5 @@ default['bonusbits_mediawiki_nginx']['aws'].tap do |aws|
   aws['az'] = node['ec2']['placement_availability_zone']
   aws['region'] = node['ec2']['placement_availability_zone'].slice(0..-2)
   aws['efs_filesystem_id'] = nil
-  aws['create_logs_group'] = true
   aws['logs_group_name'] = nil
 end
