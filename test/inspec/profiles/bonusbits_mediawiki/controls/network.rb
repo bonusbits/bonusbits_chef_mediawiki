37317fe378998953cd05e590b796d278c501f1a5
Levon Becker <levon.becker.github@bonusbits.com>
2020-01-09 14:50:52 -0800

    made rubocop happy

--- test/inspec/profiles/bonusbits_mediawiki/controls/network.rb
+++ test/inspec/profiles/bonusbits_mediawiki/controls/network.rb
@@ -10,14 +10,14 @@ control 'network' do
     it { should exist }
     it { should be_owned_by 'nginx' }
     it { should be_grouped_into 'root' }
-    its('content') { should include /^1024/ }
+    its('content') { should include '1024' }
   end
 
   describe file('/etc/sysctl.conf') do
     it { should exist }
     it { should be_owned_by 'nginx' }
     it { should be_grouped_into 'root' }
-    its('content') { should include /^net.core.somaxconn = 1024/ }
-    its('content') { should include /^net.ipv4.tcp_syncookies = 4096/ }
+    its('content') { should include 'net.core.somaxconn = 1024/' }
+    its('content') { should include 'net.ipv4.tcp_syncookies = 4096' }
   end
 end
